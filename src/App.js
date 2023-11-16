980d6983c87272a48489fa21689eb3fa87775740
Jati Putra <h4t4k4@gmail.com>
2023-11-16 22:13:55 +0700

    Changes for App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Welcome to React!</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React logo" />
+      <p>This is a simple React application.</p>
       </div>
     );
   }
