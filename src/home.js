f15ec82dc7b2f44c21defbc2145d2cbf825d6eea
Jati Putra <h4t4k4@gmail.com>
2023-11-30 21:25:00 +0700

    Customize Page

--- src/home.js
+++ src/home.js
@@ -1,62 +1,103 @@
-import React from "react"
+import React, { useState } from 'react';
 
-import { useNavigate } from "react-router-dom";
+const Home = () => {
+  const [sessionLength, setSessionLength] = useState(25); // Session duration in minutes
+  const [breakLength, setBreakLength] = useState(5); // Break duration in minutes
+  const [timeRemaining, setTimeRemaining] = useState(sessionLength * 60); // Time in seconds
+  const [isStarted, setIsStarted] = useState(false); // Indicates timer status
+  const [isBreak, setIsBreak] = useState(false); // Indicates break mode
 
+  const handleStart = () => {
+    setIsStarted(true);
+    const intervalId = setInterval(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1);
+      } else {
+        clearInterval(intervalId);
+        if (isBreak) {
+          setIsBreak(false);
+          setTimeRemaining(sessionLength * 60);
+        } else {
+          setIsBreak(true);
+          setTimeRemaining(breakLength * 60);
+        }
+      }
+    }, 1000);
+  };
 
-const Home = (props) => {
+  const handleStop = () => {
+    setIsStarted(false);
+    setTimeRemaining(sessionLength * 60);
+  };
 
-    const { loggedIn, email } = props
+  const handleReset = () => {
+    setIsStarted(false);
+    setTimeRemaining(sessionLength * 60);
+    setIsBreak(false);
+  };
 
-    const navigate = useNavigate();
-
-    
-
-    const onButtonClick = () => {
-
-        // You'll update this function later
-
+  const handleSessionChange = (event) => {
+    const newSessionLength = parseInt(event.target.value);
+    if (newSessionLength >= 1 && newSessionLength <= 60) {
+      setSessionLength(newSessionLength);
+      if (!isStarted) {
+        setTimeRemaining(newSessionLength * 60);
+      }
     }
+  };
 
+  const handleBreakChange = (event) => {
+    const newBreakLength = parseInt(event.target.value);
+    if (newBreakLength >= 1 && newBreakLength <= 60) {
+      setBreakLength(newBreakLength);
+    }
+  };
 
-    return <div className="mainContainer">
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
 
-        <div className={"titleContainer"}>
-
-            <div>Welcome!</div>
-
+  return (
+    <div className="pomodoro-timer">
+      <h1>Pomodoro Timer</h1>
+      <div className="timer-controls">
+        <div className="timer-status">
+          {isBreak ? 'Break' : 'Session'}
         </div>
-
-        <div>
-
-            This is the home page.
-
+        <div className="timer-display">
+          {formatTime(timeRemaining)}
         </div>
-
-        <div className={"buttonContainer"}>
-
-            <input
-
-                className={"inputButton"}
-
-                type="button"
-
-                onClick={onButtonClick}
-
-                value={loggedIn ? "Log out" : "Log in"} />
-
-            {(loggedIn ? <div>
-
-                Your email address is {email}
-
-            </div> : <div/>)}
-
+        <div className="timer-buttons">
+          {isStarted ? (
+            <button onClick={handleStop}>Stop</button>
+          ) : (
+            <button onClick={handleStart}>Start</button>
+          )}
+          <button onClick={handleReset}>Reset</button>
         </div>
-
-
-
+      </div>
+      <div className="timer-settings">
+        <label>Session Length:</label>
+        <input
+          type="number"
+          min="1"
+          max="60"
+          value={sessionLength}
+          onChange={handleSessionChange}
+        />
+        <label>Break Length:</label>
+        <input
+          type="number"
+          min="1"
+          max="60"
+          value={breakLength}
+          onChange={handleBreakChange}
+        />
+      </div>
     </div>
+  );
+};
 
-}
-
-
-export default Home
+export default Home;
